520863f9717cb073cf6a99b6caf814f757ae6aac
Baby Jane Barrun <babyjanebarrun@gmail.com>
2026-04-23 10:20:18 +0800

    Update 1_🏠_Home.py

--- portfolio_app/pages/1_🏠_Home.py
+++ portfolio_app/pages/1_🏠_Home.py
@@ -4,6 +4,6 @@ st.title("🏠 Home")
 
 st.header("Hi, I'm Baby Jane 👋")
 st.write("Computer Science Student | Future Developer💻")
-st.image("c:\Users\Renbee Misamen\OneDrive\Documents\image.jpeg", caption="My Banner", width=300)
+st.image("portfolio_app/image.jpeg", caption="My Banner", width=300)
 
 st.info("Welcome to my portfolio! Explore my work and skills.")
